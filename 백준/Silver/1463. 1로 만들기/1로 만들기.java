422563b31ffff1743c181451181794299834da12
MinHyeok Lee <minhyeok12.dev@gmail.com>
2025-10-22 22:05:30 +0900

    [Silver III] Title: 1로 만들기, Time: 108 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(recur(N, 0));
+    }
+    static int recur(int N, int count){
+        if( N < 2)
+            return count;
+        return Math.min(recur( N/2, count + 1 + N % 2), recur( N / 3, count + 1 + N % 3));
+    }
+}
