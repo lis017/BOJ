13a289b002ad8d95cfe27e2b63e95ece456ae342
MinHyeok Lee <minhyeok12.dev@gmail.com>
2026-02-10 19:20:57 +0900

    [Gold II] Title: 가운데를 말해요, Time: 516 ms, Memory: 40916 KB -BaekjoonHub

---
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		
+		Queue<Integer> maxHeap = new PriorityQueue<>((o1,o2) -> o2-o1); // 내림차순  
+		Queue<Integer> minHeap = new PriorityQueue<>((o1,o2) -> o1-o2); // 오름차순 
+		
+		for(int i=0; i<n; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if(maxHeap.size() == minHeap.size()) maxHeap.add(num);
+			else minHeap.add(num);
+            
+			// maxHeap이 더 클 경우 원소 switch
+			if(!maxHeap.isEmpty() && !minHeap.isEmpty()) {
+				if(maxHeap.peek() > minHeap.peek()) {
+					int tmp = maxHeap.poll();
+					maxHeap.offer(minHeap.poll());
+					minHeap.offer(tmp);
+				}
+			}
+			sb.append(maxHeap.peek()+"\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
