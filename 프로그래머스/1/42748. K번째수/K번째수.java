6be556a98d8292d5d160a3a8cf3310af709aef67
MinHyeok Lee <minhyeok12.dev@gmail.com>
2026-03-16 20:12:30 +0900

    [level 1] Title: K번째수, Time: 1.45 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int [commands.length];
+        for ( int i = 0; i < commands.length; i++){
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int target = commands[i][2];
+            int[] temp = Arrays.copyOfRange(array, start - 1, end);
+            Arrays.sort(temp);
+            answer[i] = temp[target - 1];
+        }
+        return answer;
+    }
+}
