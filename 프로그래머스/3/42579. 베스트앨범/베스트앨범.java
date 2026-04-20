eed622d65bf422fcaf397f5645d1e28fc9c310ee
MinHyeok Lee <minhyeok12.dev@gmail.com>
2026-04-20 23:11:55 +0900

    [level 3] Title: 베스트앨범, Time: 5.20 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,58 +1,6 @@
-import java.util.*;
-
 class Solution {
-    static class Song {
-        String genre;
-        int playCount;
-        int index;
-        
-        Song (String genre, int playCount, int index) {
-            this.genre = genre;
-            this.playCount = playCount;
-            this.index = index;
-        }
-    }
     public int[] solution(String[] genres, int[] plays) {
-        Map<String, Integer> totalPlayCountByGenre = new HashMap<>();
-        Map<String, List<Song>> songsByGenre = new HashMap<>();
-        
-        for (int index = 0; index < genres.length; index++){
-            String genre = genres[index];
-            int playCount = plays[index];
-            
-            totalPlayCountByGenre.put(
-                genre,
-                totalPlayCountByGenre.getOrDefault(genre, 0) + playCount
-            );
-            
-            songsByGenre.putIfAbsent(genre, new ArrayList<>());
-            songsByGenre.get(genre).add(new Song(genre, playCount, index));
-        }
-        
-        List<String> genreOrder = new ArrayList<>(totalPlayCountByGenre.keySet());
-        genreOrder.sort((genre1, genre2) -> 
-                       totalPlayCountByGenre.get(genre2) - totalPlayCountByGenre.get(genre1)
-                       );
-        
-        List<Integer> result = new ArrayList<>();
-        
-        for(String genre : genreOrder) {
-            List<Song> songs = songsByGenre.get(genre);
-            
-            songs.sort((song1, song2) -> {
-                if( song1.playCount == song2.playCount){
-                    return song1.index - song2.index;
-                }
-                return song2.playCount - song1.playCount;
-            });
-            
-            result.add(songs.get(0).index);
-            
-            if(songs.size() >= 2){
-                result.add(songs.get(1).index);
-            }
-        }
-            
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = {};
+        return answer;
     }
 }
