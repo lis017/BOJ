c2a0f724bbfb75e640084abb95a3726371c46071
MinHyeok Lee <minhyeok12.dev@gmail.com>
2026-04-20 20:31:35 +0900

    [level 2] Title: 전화번호 목록, Time: 328.72 ms, Memory: 97.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        
+        for(int index = 0; index < phone_book.length - 1; index++){
+            if(phone_book[index + 1].startsWith(phone_book[index])){
+                return false;
+            }
+        }
+        return true;
+    }
+}
