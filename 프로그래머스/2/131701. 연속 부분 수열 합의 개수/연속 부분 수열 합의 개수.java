1c29f8689bb467ee0bbc72158f55c0d56867fd09
MinHyeok Lee <minhyeok12.dev@gmail.com>
2026-05-09 11:08:33 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 63.49 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,23 +1,34 @@
+/*
+유형: set, 슬라이딩윈도우?
+
+1분설계: 반복문에 갯수만큼 더하고, set넣고
+    다음으로 left right 둘다 옮겨가며 set에 저장. 끝
+*/
 import java.io.*;
 import java.util.*;
 
 class Solution {
     public int solution(int[] elements) {
-        Set<Integer> answer = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
         int n = elements.length;
         
-        for(int length = 1; length <= n; length++){
+        //반복문
+        for(int size = 1; size <= n; size++){
+            //? ~ i까지 sum에 더할거야.
             int sum = 0;
-            for(int index = 0; index < length; index++){
-                sum += elements[index];
+            for(int curlast = 0; curlast < size; curlast++){
+                sum += elements[curlast];
             }
-            answer.add(sum);
-            for(int start = 1; start < n; start++){
-                sum -= elements[start - 1];
-                sum += elements[(start + length - 1) % n];
-                answer.add(sum);
+            set.add(sum);
+            
+            //반복문
+            //원래 첫째 뺴고, 다음거 sum에 더하고 set에 넣고를 1 ~ < n ?
+            for(int index = 1; index < n; index++){
+                sum -= elements[index];
+                sum += elements[(size + index - 1) % n];
+                set.add(sum);
             }
         }
-        return answer.size();
+        return set.size();
     }
 }
