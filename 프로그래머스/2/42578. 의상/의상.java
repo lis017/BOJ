89d3db5f896d48618f681665efe56f868371fcaf
MinHyeok Lee <minhyeok12.dev@gmail.com>
2026-06-25 00:17:00 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,14 +4,13 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> map = new HashMap<>();
-        
-        for( String[] clothe : clothes){
-            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1 );
+        for (String[] clothe : clothes){
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
         }
-        int count = 1;
-        for(String c : map.keySet() ){
-            count *= map.get(c) + 1;
+        int answer = 1;
+        for( String clo : map.keySet() ){
+            answer *= (map.get(clo) + 1);
         }
-        return count - 1;
+        return answer - 1;
     }
 }
