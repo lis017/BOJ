8b8d4501d05a03eb7feb40b34a928dd9966c834f
MinHyeok Lee <minhyeok12.dev@gmail.com>
2026-06-30 23:36:51 +0900

    [level 3] Title: 정수 삼각형, Time: 6.01 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -4,29 +4,34 @@ import java.util.*;
 class Solution {
     public int solution(int[][] triangle) {
         int n = triangle.length;
-        int maxm = triangle[n-1].length;
-        int[][] dp = new int[n][maxm];
-        for(int row = 0; row < n; row++){
-            int m = triangle[row].length;
-            for(int col = 0; col < m; col++){
+        int m = triangle[n-1].length;
+        int[][] dp = new int[n][m];
+        
+        for( int row = 0; row < n; row++){
+            for ( int col = 0; col <= row; col++){
                 if ( row == 0 && col == 0){
                     dp[row][col] = triangle[row][col];
+                    //System.out.println(dp[row][col]);
                 }
                 else if ( col == 0){
-                    dp[row][col] = dp[row - 1][col] + triangle[row][col];
-                }else if ( col == m-1){
-                    dp[row][col] = dp[row - 1][col - 1] + triangle[row][col];
+                    dp[row][col] = triangle[row][col] + dp[row - 1][col];
+                    //System.out.println(dp[row][col]);
+                }
+                else if ( row == col){
+                    dp[row][col] = triangle[row][col] + dp[row-1][col - 1];
+                    //System.out.println(dp[row][col]);
                 }
                 else{
-                    dp[row][col] = Math.max(dp[row - 1][col - 1], dp[row - 1][col]) + triangle[row][col];
+                    dp[row][col] = triangle[row][col] + Math.max(dp[row-1][col-1], dp[row-1][col]);
+                    //System.out.println(dp[row][col]);
                 }
+                    
             }
         }
-        int max = 0;
-        //row n-1일때 싹 보고 최댓값을 구해야돼.
-        for(int col = 0; col < n; col++){
-            max = Math.max(dp[n-1][col], max);
+        int answer = 0;
+        for( int i = 0; i < n; i++){
+            answer = Math.max(answer, dp[n-1][i]);
         }
-        return max;
+        return answer;
     }
 }
