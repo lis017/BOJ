2812392975f0858a2ec67ee59d7568b736be9e4a
MinHyeok Lee <minhyeok12.dev@gmail.com>
2026-07-06 20:27:03 +0900

    [level 2] Title: 소수 찾기, Time: 7.96 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,42 +2,44 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    int n;
     boolean[] visited;
+    int n;
     Set<Integer> set = new HashSet<>();
     public int solution(String numbers) {
-        int answer = 0;
         n = numbers.length();
         visited = new boolean[n];
+        int answer = 0;
         dfs(numbers, "", 0);
-        for ( int num : set){
-            System.out.println(num);
-            if ( isPrime(num)){
+        //System.out.println("set = " + set);
+        for( int s : set){
+            if(isPrime(s)){
                 answer++;
             }
         }
         return answer;
     }
-    void dfs(String numbers, String sum, int depth){
-        for( int i = 0; i < n; i++){
-            if ( !visited[i]){
+    void dfs(String numbers, String number, int depth){
+        for( int i = 0; i < n; i ++){
+            if ( !visited[i] ){
                 visited[i] = true;
-                dfs(numbers, sum + numbers.charAt(i), depth+1);
+                dfs(numbers, number + numbers.charAt(i), depth + 1);
                 visited[i] = false;
             }
-
         }
-        if ( sum != ""){
-            set.add(Integer.parseInt(sum));
+        if ( !number.isEmpty() ){
+            set.add(Integer.parseInt(number) );
         }
     }
-    boolean isPrime(int num){
-        if ( num <= 1) return false;
-        for(int i = 2; i * i <= num; i++){
-            if ( num % i == 0 ){
-                return false;
-            } 
+    boolean isPrime(int prime){
+        if ( prime <= 1){
+            return false;
         }
+        for( int i = 2; i * i <= prime; i++){
+            if ( prime % i == 0 ) {
+                return false;
+            }
+        }
+        //System.out.println("prime = " + prime);
         return true;
     }
 }
